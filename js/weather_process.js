378ef0395cc25c06f5480f3a5e7346ef42b95c7f
yunnysunny <yunnysunny@gmail.com>
2013-03-20 17:42:30 +0800

    增加了后三天的天气显示

--- js/weather_process.js
+++ js/weather_process.js
@@ -1,3 +1,4 @@
+var IMG_BASE_URL = 'http://m.weather.com.cn/img/b';
 function getWeather(cityObj) {
 	if (typeof cityObj == 'object')
 	{
@@ -10,11 +11,25 @@ function getWeather(cityObj) {
 				var weatherInfo = obj.weatherinfo;
 
 				SJS.$('dateNow').innerHTML = weatherInfo.date_y + '(' + weatherInfo.week + ')';
-				SJS.$('weathDescription').innerHTML = weatherInfo.img_title1;
+				SJS.$('weathDescription').innerHTML = weatherInfo.weather1;
 				SJS.$('city').innerHTML = weatherInfo.city;
-				SJS.$('weatherImg').setAttribute('src','http://m.weather.com.cn/img/b'+weatherInfo.img1+'.gif');
+				SJS.$('weatherImg1').setAttribute('src',IMG_BASE_URL+weatherInfo.img1+'.gif');
+				SJS.$('weatherImg2').setAttribute('src',IMG_BASE_URL+weatherInfo.img2+'.gif');
 				SJS.$('temp').innerHTML = weatherInfo.temp1;
 				SJS.$('wind').innerHTML = weatherInfo.wind1;
+
+				var days = SJS.getElementsByClassName(SJS.$('otherDates'),'div','sub2');
+				for (var i=0,len=days.length;i<len ;i++ )
+				{
+					var dayNow = days[i];
+					var imgs = dayNow.getElementsByTagName('img');
+					imgs[0].setAttribute('src',IMG_BASE_URL + weatherInfo['img'+((i+2)*2-1)] + '.gif');
+					imgs[1].setAttribute('src',IMG_BASE_URL + weatherInfo['img'+((i+2)*2)] + '.gif');
+					SJS.getElementsByClassName(dayNow,'span','temp')[0].innerHTML = weatherInfo['temp'+(i+2)];
+					SJS.getElementsByClassName(dayNow,'span','description')[0].innerHTML = weatherInfo['weather'+(i+2)];
+					SJS.getElementsByClassName(dayNow,'span','wind')[0].innerHTML = weatherInfo['wind'+(i+2)];
+				}				
+				
 				SJS.show(SJS.$('container'));
 			}
 			else
