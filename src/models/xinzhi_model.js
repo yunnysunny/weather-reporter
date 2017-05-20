53b3177986bb7643641e9aa6c6cc91e4f4ca95b7
yunnysunny <yunnysunny@gmail.com>
2017-05-20 15:37:41 +0800

    实现心知接口签名逻辑，重构渲染代码

--- src/models/xinzhi_model.js
+++ src/models/xinzhi_model.js
@@ -2,10 +2,10 @@ var crypto = require('crypto');
 var config = require('../config');
 const UID = config.xinzhiUid;
 const KEY = config.xinzhiKey;
-const TTL = 600;
+const TTL = 2592000;
 
 
-exports.genSign = function() {
+exports.genSign = function() {console.log(KEY,'key',UID,'uid');
     var ts = Math.floor(new Date().getTime() / 1000);
     var str = 'ts=' + ts + '&ttl=' + TTL + '&uid=' + UID;
     var sign = crypto.createHmac('sha1',KEY).update(str).digest('base64');
--- src/public/js/weather_process2.js
+++ src/public/js/weather_process2.js
@@ -8,9 +8,9 @@ The above copyright notice and this permission notice shall be included in all c
 * @author gaoyang  <yunnysunny@gmail.com>
 * @date 2017.1.9
 */
-var BASE_URL_API = 'https://api.thinkpage.cn/v3/weather/daily.json';
+var BASE_URL_API = 'https://api.seniverse.com/v3/weather/daily.json';
 var UID = 'UE6651266E';
-var TTL = 600;
+var TTL = 2592000;
 var BASE_URL_IMG = 'https://www.thinkpage.cn';
 var MAP_WEATHER_IMG = { "晴": "/weather/images/icons/3d_50/3.png", "多云": "/weather/images/icons/3d_50/4.png", "晴间多云": "/weather/images/icons/3d_50/6.png", "大部多云": "/weather/images/icons/3d_50/8.png", "阴": "/weather/images/icons/3d_50/9.png", "阵雨": "/weather/images/icons/3d_50/10.png", "雷阵雨": "/weather/images/icons/3d_50/11.png", "雷阵雨伴有冰雹": "/weather/images/icons/3d_50/12.png", "小雨": "/weather/images/icons/3d_50/13.png", "中雨": "/weather/images/icons/3d_50/14.png", "大雨": "/weather/images/icons/3d_50/15.png", "暴雨": "/weather/images/icons/3d_50/16.png", "大暴雨": "/weather/images/icons/3d_50/17.png", "特大暴雨": "/weather/images/icons/3d_50/18.png", "冻雨": "/weather/images/icons/3d_50/19.png", "雨夹雪": "/weather/images/icons/3d_50/20.png", "阵雪": "/weather/images/icons/3d_50/21.png", "小雪": "/weather/images/icons/3d_50/22.png", "中雪": "/weather/images/icons/3d_50/23.png", "大雪": "/weather/images/icons/3d_50/24.png", "暴雪": "/weather/images/icons/3d_50/25.png", "浮尘": "/weather/images/icons/3d_50/26.png", "扬沙": "/weather/images/icons/3d_50/27.png", "沙尘暴": "/weather/images/icons/3d_50/28.png", "强沙尘暴": "/weather/images/icons/3d_50/29.png", "雾": "/weather/images/icons/3d_50/30.png", "霾": "/weather/images/icons/3d_50/31.png", "风": "/weather/images/icons/3d_50/32.png", "大风": "/weather/images/icons/3d_50/33.png", "飓风": "/weather/images/icons/3d_50/34.png", "热带风暴": "/weather/images/icons/3d_50/35.png", "龙卷风": "/weather/images/icons/3d_50/36.png", "冷": "/weather/images/icons/3d_50/37.png", "热": "/weather/images/icons/3d_50/38.png", "未知": "/weather/images/icons/3d_50/99.png" };
 
@@ -18,33 +18,49 @@ function __Img(text) {
     return BASE_URL_IMG + MAP_WEATHER_IMG[text];
 }
 
+function renderWeather(container,info) {
+
+    var getOneElementByClassName = function(tag,className) {
+        return SJS.getElementsByClassName(container,tag,className)[0];
+    } ;
+    var descriptionDom = getOneElementByClassName('span','js-description') ;
+    var img1Dom = getOneElementByClassName('img','js-weather-img1');
+    var img2Dom = getOneElementByClassName('img','js-weather-img2');
+    var tempDom = getOneElementByClassName('span','js-temp');
+    var windDom = getOneElementByClassName('span','js-wind');
+
+    tempDom.innerHTML = info.low + '到' + info.high + '℃';
+    var des = '';
+    var dayText = info.text_day;
+    var nightText = info.text_night;
+    if (dayText != nightText) {
+        img1Dom.setAttribute('src', __Img(dayText));
+        img2Dom.setAttribute('src', __Img(nightText));
+        descriptionDom.innerHTML = dayText + '转' + nightText;
+    } else {
+        img1Dom.setAttribute('src', __Img(dayText));
+        descriptionDom.innerHTML = dayText;
+    }
+    
+    windDom.innerHTML = '风力'+info.wind_scale+'级';
+}
+
 function getWeather(city) {
     var signData = SJS.$('sign-data');
-    var params = 'ts=' + signData.getAttribute('ts') + '&ttl=' + TTL + '&uid=' + UID + '&sign=' + signData.getAttribute('sign') + '&location=' + encodeURIComponent(city) + '&days=4';
-    //var params = 'key=dkpjjetqan4gyllr&location=' + encodeURIComponent(city) + '&days=4';
+    var params = 'ts=' + signData.getAttribute('ts') + '&ttl=' + TTL + '&uid=' + UID + '&sig=' + signData.getAttribute('sign') + '&location=' + encodeURIComponent(city) + '&days=4&start=0';
     SJS.sendRequest('get', BASE_URL_API, params, function(data) {
 
         var obj = data.results[0];
         var weatherInfo = obj.daily;
         var todayWeather = weatherInfo[0];
-        SJS.$('dateNow').innerHTML = todayWeather.date;
-        SJS.$('weathDescription').innerHTML = todayWeather.text_day + '-' + todayWeather.text_night;
-        SJS.$('city').innerHTML = city;
-        SJS.$('weatherImg1').setAttribute('src', __Img(todayWeather.text_day));
-        SJS.$('weatherImg2').setAttribute('src', __Img(todayWeather.text_night));
-        SJS.$('temp').innerHTML = todayWeather.low + '到' + todayWeather.high;
-        SJS.$('wind').innerHTML = todayWeather.wind_scale;
+        SJS.$('dateNow').innerHTML = todayWeather.date;SJS.$('city').innerHTML = city;
+        renderWeather(SJS.$('today'),todayWeather);
 
         var days = SJS.getElementsByClassName(SJS.$('otherDates'), 'div', 'sub2');
         for (var i = 1, len = days.length; i < len; i++) {
-            var dayNow = days[i];
+            var dayNow = days[i-1];
             var info = weatherInfo[i];
-            var imgs = dayNow.getElementsByTagName('img');
-            imgs[0].setAttribute('src', __Img(info.text_day));
-            imgs[1].setAttribute('src', __Img(info.text_night));
-            SJS.getElementsByClassName(dayNow, 'span', 'temp')[0].innerHTML = info.low + '到' + info.high;;
-            SJS.getElementsByClassName(dayNow, 'span', 'description')[0].innerHTML = info.text_day + '-' + info.text_night;
-            SJS.getElementsByClassName(dayNow, 'span', 'wind')[0].innerHTML = info.wind_scale;
+            renderWeather(dayNow,info);
         }
 
         SJS.show(SJS.$('container'));
